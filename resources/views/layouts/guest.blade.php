c6b41c3a60499b1286072cae19567a8769e3a222
Kim Hallberg <hallberg.kim@gmail.com>
2023-08-06 05:26:38 +0200

    Add Livewire v2 Blade directives

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,9 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Livewire -->
+        @livewireStyles
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
@@ -26,5 +29,8 @@
                 {{ $slot }}
             </div>
         </div>
+
+        <!-- Livewire -->
+        @livewireScripts
     </body>
 </html>
